0d4c8cbb0e3b31b4e2355fc7f8b8631a18c19ab6
Valerio Gallina <valeriudus@hotmail.it>
2023-09-12 15:56:07 +0200

    creazione annuncio

---
+++ resources/views/announcement/create.blade.php
@@ -0,0 +1,28 @@
+<x-layout>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2>Aggiungi il tuo videogame</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-4">
+                <livewire:create-announcement/>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <x-layout />
