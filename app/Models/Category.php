f2273a29810e8f24c9021867d9b7a1077f6a2923
Piertobia Nigro <ngrptb@gmail.com>
2023-09-13 10:52:46 +0200

    Relazioni 1 a N annunci-categoria. Inserimento categoria completato

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Announcement;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
@@ -12,4 +14,9 @@ class Category extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function announcements(): HasMany
+    {
+        return $this->hasMany(Announcement::class);
+    }
 }
--- database/migrations/2023_09_12_143557_create_categories_table.php
+++ database/migrations/2023_09_12_143557_create_categories_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Category;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -16,6 +17,23 @@ return new class extends Migration
             $table->string('name');
             $table->timestamps();
         });
+
+        $categories = [
+            "Azione",
+            "MMORPG",
+            "Sparatutto",
+            "Survival",
+            "Puzzle",
+            "Strategia",
+            "RPG",
+            "Simulatori",
+            "Sport"
+        ];
+        foreach ($categories as $category) {
+            Category::create([
+               'name'=> $category
+             ]);
+         }
     }
 
     /**
