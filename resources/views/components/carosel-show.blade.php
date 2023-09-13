608370ee8c5ca6b4a27a75e4e933d123d2bc59fc
Piertobia Nigro <ngrptb@gmail.com>
2023-09-13 17:01:26 +0200

    link home sul logo navbar

--- resources/views/components/carosel-show.blade.php
+++ resources/views/components/carosel-show.blade.php
@@ -1,13 +1,13 @@
 <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active" data-bs-interval="2000">
-      <img src="https://picsum.photos/500" class="d-block w-75 h-50" alt="...">
+      <img src="https://picsum.photos/500" class="d-block w-100 h-50" alt="...">
     </div>
     <div class="carousel-item" data-bs-interval="2000">
-      <img src="https://picsum.photos/500" class="d-block w-75 h-50" alt="...">
+      <img src="https://picsum.photos/500" class="d-block w-100 h-50" alt="...">
     </div>
     <div class="carousel-item" data-bs-interval="2000">
-      <img src="https://picsum.photos/500" class="d-block w-75 h-50" alt="...">
+      <img src="https://picsum.photos/500" class="d-block w-100 h-50" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
