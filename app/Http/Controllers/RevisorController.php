2a50f77d19f6418b1bbadd891451853eb8c677a8
Piertobia Nigro <ngrptb@gmail.com>
2023-09-14 18:23:31 +0200

    Implemtentazione servizio mail. Configurazione mail. Risolvere l'errore maledetto

--- app/Http/Controllers/RevisorController.php
+++ app/Http/Controllers/RevisorController.php
@@ -2,8 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Mail\BecomeRevisor;
 use App\Models\Announcement;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Artisan;
+
 
 class RevisorController extends Controller
 {
@@ -20,5 +27,15 @@ class RevisorController extends Controller
     public function rejectAnnouncement(Announcement $announcement){
         $announcement->setAccepted(false);
         return redirect()->back()->with('message',"Hai rifiutato l'articolo di {$announcement->user->name}");
+    }
+
+    public function becomeRevisor(){
+        Mail::to('hello@example.com')->send(new BecomeRevisor(Auth::user()));
+        return redirect()->back()->with('message', 'La tua candidatura è stata inviata con successo.');
+    }
+
+    public function makeRevisor(User $user){
+        Artisan::call('retro:makeUserRevisor', ["email"=>$user->email]);
+        return redirect()->back()->with('message', "L'utente è stato correttamente promosso a revisore.");
     }
 }
---
+++ resources/views/mail/become_revisor.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Retro Hunters Support</title>
+</head>
+<body>
+    <div>
+        <h2>Un utente si è candidato al ruolo di revisore</h2>
+        <h3>I suoi dati sono: </h3>
+        <p>Nome: {{$user->name}}</p>
+        <p>Email: {{$user->email}}</p>
+        <p>Se vuoi renderlo revisore clicca qui</p>
+        <a href="{{route('make.revisor', compact('user'))}}">Rendi revisore</a>
+    </div>
+</body>
+</html>
