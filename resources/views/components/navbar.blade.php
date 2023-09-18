34c6ecdbab8f3c07201a91f6b72ec0b0a2a6dce3
Piertobia Nigro <ngrptb@gmail.com>
2023-09-18 17:55:57 +0200

    frontend navbar login, register, logout

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-custom">
+<nav class="navbar navbar-expand-lg navbar-custom py-3">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ route('home') }}">RH</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
@@ -35,63 +35,66 @@
                 </li>
 
             </ul>
-        </div>
-        @auth
-            <p class="d-none">
-                {{ $counter = App\Models\Announcement::toBeRevisionedCount() }} 
-            </p>
-            <li class="nav-item dropdown ms-auto mx-5">
-                <a class="nav-link link-custom dropdown-toggle mx-2" href="#" role="button" data-bs-toggle="dropdown"
-                    aria-expanded="false">
-                    {{ Auth::user()->name }}
-                    @if ($counter > 0)
-                        <span class="badge rounded-pill bg-danger">
-                            {{ App\Models\Announcement::toBeRevisionedCount() }}
-                            <span class="visually-hidden">messaggi non letti</span>
-                        </span>
-                    @endif
-                </a>
-                <ul class="dropdown-menu">
-                    @if (Auth::user()->is_revisor)
-                        <li class="nav-item">
-                            <a class="nav-link btn btn-sm link-custom" href="{{ route('revisor.index') }}">Area Revisioni
-                                <span class="badge rounded-pill bg-danger">
-                                    {{ App\Models\Announcement::toBeRevisionedCount() }}
-                                    <span class="visually-hidden">messaggi non letti</span>
-                                </span>
-                            </a>
+            @auth
+                <p class="d-none">
+                    {{ $counter = App\Models\Announcement::toBeRevisionedCount() }}
+                </p>
+                <li class="nav-item dropdown ms-auto mx-5">
+                    <a class="nav-link link-custom dropdown-toggle mx-2" href="#" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        {{ Auth::user()->name }}
+                        @if ($counter > 0)
+                            <span class="badge rounded-pill bg-danger">
+                                {{ App\Models\Announcement::toBeRevisionedCount() }}
+                                <span class="visually-hidden">messaggi non letti</span>
+                            </span>
+                        @endif
+                    </a>
+                    <ul class="dropdown-menu">
+                        @if (Auth::user()->is_revisor)
+                            <li class="nav-item">
+                                <a class="nav-link btn btn-sm link-custom" href="{{ route('revisor.index') }}">Area
+                                    Revisioni
+                                    <span class="badge rounded-pill bg-danger">
+                                        {{ App\Models\Announcement::toBeRevisionedCount() }}
+                                        <span class="visually-hidden">messaggi non letti</span>
+                                    </span>
+                                </a>
+                            </li>
+                        @endif
+                        <hr class="mx-2">
+                        <li class="nav-item mx-2">
+                            <a href="/logout"
+                                onclick="event.preventDefault();getElementById('form-logout').submit()">Logout</a>
                         </li>
-                    @endif
-                    <hr class="mx-2">
-                    <li class="nav-item mx-2">
-                        <a href="/logout" onclick="event.preventDefault();getElementById('form-logout').submit()">Logout</a>
-                    </li>
-                    <form id="form-logout" action="{{ route('logout') }}" method="POST">
-                        @csrf
-                    </form>
-                </ul>
-            </li>
-        @endauth
+                        <form id="form-logout" action="{{ route('logout') }}" method="POST">
+                            @csrf
+                        </form>
+                    </ul>
+                </li>
+            @endauth
+            @guest
+                <li class="nav-item dropdown ms-auto">
+                    <a class="nav-link link-custom dropdown-toggle mx-2" href="#" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Visitatore
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="nav-item mx-2">
+                            <a href="/login"
+                                onclick="event.preventDefault();getElementById('form-login').submit()">Login</a>
+                        </li>
+                        <form id="form-login" action="{{ route('login') }}" method="GET">
+                        </form>
+                        <li class="nav-item mx-2">
+                            <a href="/register"
+                                onclick="event.preventDefault();getElementById('form-register').submit()">Registrati</a>
+                        </li>
+                        <form id="form-register" action="{{ route('register') }}" method="GET">
+                        </form>
+                    </ul>
+                </li>
+            @endguest
+        </div>
     </div>
-    @guest
-        <li class="nav-item dropdown ms-auto">
-            <a class="nav-link link-custom dropdown-toggle mx-2" href="#" role="button" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Visitatore
-            </a>
-            <ul class="dropdown-menu">
-                <li class="nav-item mx-2">
-                    <a href="/login" onclick="event.preventDefault();getElementById('form-login').submit()">Login</a>
-                </li>
-                <form id="form-login" action="{{ route('login') }}" method="GET">
-                </form>
-                <li class="nav-item mx-2">
-                    <a href="/register"
-                        onclick="event.preventDefault();getElementById('form-register').submit()">Registrati</a>
-                </li>
-                <form id="form-register" action="{{ route('register') }}" method="GET">
-                </form>
-            </ul>
-        </li>
-    @endguest
 </nav>
--- storage/framework/views/786debc48f215dc0bc806e95ee10701c.php
+++ storage/framework/views/786debc48f215dc0bc806e95ee10701c.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-custom">
+<nav class="navbar navbar-expand-lg navbar-custom py-3">
     <div class="container-fluid">
         <a class="navbar-brand" href="<?php echo e(route('home')); ?>">RH</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
@@ -35,67 +35,71 @@
                 </li>
 
             </ul>
-        </div>
-        <?php if(auth()->guard()->check()): ?>
-            <p class="d-none">
-                <?php echo e($counter = App\Models\Announcement::toBeRevisionedCount()); ?> 
-            </p>
-            <li class="nav-item dropdown ms-auto mx-5">
-                <a class="nav-link link-custom dropdown-toggle mx-2" href="#" role="button" data-bs-toggle="dropdown"
-                    aria-expanded="false">
-                    <?php echo e(Auth::user()->name); ?>
+            <?php if(auth()->guard()->check()): ?>
+                <p class="d-none">
+                    <?php echo e($counter = App\Models\Announcement::toBeRevisionedCount()); ?>
 
-                    <?php if($counter > 0): ?>
-                        <span class="badge rounded-pill bg-danger">
-                            <?php echo e(App\Models\Announcement::toBeRevisionedCount()); ?>
+                </p>
+                <li class="nav-item dropdown ms-auto mx-5">
+                    <a class="nav-link link-custom dropdown-toggle mx-2" href="#" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        <?php echo e(Auth::user()->name); ?>
 
-                            <span class="visually-hidden">messaggi non letti</span>
-                        </span>
-                    <?php endif; ?>
-                </a>
-                <ul class="dropdown-menu">
-                    <?php if(Auth::user()->is_revisor): ?>
-                        <li class="nav-item">
-                            <a class="nav-link btn btn-sm link-custom" href="<?php echo e(route('revisor.index')); ?>">Area Revisioni
-                                <span class="badge rounded-pill bg-danger">
-                                    <?php echo e(App\Models\Announcement::toBeRevisionedCount()); ?>
+                        <?php if($counter > 0): ?>
+                            <span class="badge rounded-pill bg-danger">
+                                <?php echo e(App\Models\Announcement::toBeRevisionedCount()); ?>
 
-                                    <span class="visually-hidden">messaggi non letti</span>
-                                </span>
-                            </a>
+                                <span class="visually-hidden">messaggi non letti</span>
+                            </span>
+                        <?php endif; ?>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <?php if(Auth::user()->is_revisor): ?>
+                            <li class="nav-item">
+                                <a class="nav-link btn btn-sm link-custom" href="<?php echo e(route('revisor.index')); ?>">Area
+                                    Revisioni
+                                    <span class="badge rounded-pill bg-danger">
+                                        <?php echo e(App\Models\Announcement::toBeRevisionedCount()); ?>
+
+                                        <span class="visually-hidden">messaggi non letti</span>
+                                    </span>
+                                </a>
+                            </li>
+                        <?php endif; ?>
+                        <hr class="mx-2">
+                        <li class="nav-item mx-2">
+                            <a href="/logout"
+                                onclick="event.preventDefault();getElementById('form-logout').submit()">Logout</a>
                         </li>
-                    <?php endif; ?>
-                    <hr class="mx-2">
-                    <li class="nav-item mx-2">
-                        <a href="/logout" onclick="event.preventDefault();getElementById('form-logout').submit()">Logout</a>
-                    </li>
-                    <form id="form-logout" action="<?php echo e(route('logout')); ?>" method="POST">
-                        <?php echo csrf_field(); ?>
-                    </form>
-                </ul>
-            </li>
-        <?php endif; ?>
+                        <form id="form-logout" action="<?php echo e(route('logout')); ?>" method="POST">
+                            <?php echo csrf_field(); ?>
+                        </form>
+                    </ul>
+                </li>
+            <?php endif; ?>
+            <?php if(auth()->guard()->guest()): ?>
+                <li class="nav-item dropdown ms-auto">
+                    <a class="nav-link link-custom dropdown-toggle mx-2" href="#" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Visitatore
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="nav-item mx-2">
+                            <a href="/login"
+                                onclick="event.preventDefault();getElementById('form-login').submit()">Login</a>
+                        </li>
+                        <form id="form-login" action="<?php echo e(route('login')); ?>" method="GET">
+                        </form>
+                        <li class="nav-item mx-2">
+                            <a href="/register"
+                                onclick="event.preventDefault();getElementById('form-register').submit()">Registrati</a>
+                        </li>
+                        <form id="form-register" action="<?php echo e(route('register')); ?>" method="GET">
+                        </form>
+                    </ul>
+                </li>
+            <?php endif; ?>
+        </div>
     </div>
-    <?php if(auth()->guard()->guest()): ?>
-        <li class="nav-item dropdown ms-auto">
-            <a class="nav-link link-custom dropdown-toggle mx-2" href="#" role="button" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Visitatore
-            </a>
-            <ul class="dropdown-menu">
-                <li class="nav-item mx-2">
-                    <a href="/login" onclick="event.preventDefault();getElementById('form-login').submit()">Login</a>
-                </li>
-                <form id="form-login" action="<?php echo e(route('login')); ?>" method="GET">
-                </form>
-                <li class="nav-item mx-2">
-                    <a href="/register"
-                        onclick="event.preventDefault();getElementById('form-register').submit()">Registrati</a>
-                </li>
-                <form id="form-register" action="<?php echo e(route('register')); ?>" method="GET">
-                </form>
-            </ul>
-        </li>
-    <?php endif; ?>
 </nav>
 <?php /**PATH C:\Users\amste\wa\Retro-Hunters_gruppo_01\resources\views/components/navbar.blade.php ENDPATH**/ ?>
