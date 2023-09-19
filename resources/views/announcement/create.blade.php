347a266f2ab924d661d4e32a5cc0ef61c9e45bf5
MarcoCz88 <138657270+MarcoCz88@users.noreply.github.com>
2023-09-19 12:14:51 +0200

    pagina aggiunti videogames modificata

--- resources/views/announcement/create.blade.php
+++ resources/views/announcement/create.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
     <div class="container">
         <div class="row">
-            <div class="col-12 mt-3 text-primary">
-                <h2>Aggiungi il tuo videogame: </h2>
+            <div class="col-6 mt-3 text-center rounded-top rounded-bottom text-primary border-bottom border-top border-warning">
+                <h2 class="my-4">Aggiungi il tuo videogame: </h2>
             </div>
         </div>
         <div class="row">
