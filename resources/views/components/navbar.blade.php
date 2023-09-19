5083019a813a801e776aff7fddc0e386c74b434b
Piertobia Nigro <ngrptb@gmail.com>
2023-09-19 22:27:10 +0200

    pull

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -97,12 +97,12 @@
                     </ul>
                 </li>
             @endguest
-            <li class="nav-item"><x-_locale lang="it" nation="it"/></li>
-            <li class="nav-item"><x-_locale lang="en" nation="gb"/></li>
-            <li class="nav-item"><x-_locale lang="es" nation="es"/></li>
+            <li class="nav-item"><x-_locale lang="it" nation="it" /></li>
+            <li class="nav-item"><x-_locale lang="en" nation="gb" /></li>
+            <li class="nav-item"><x-_locale lang="es" nation="es" /></li>
         </div>
-        
-        
+
+
 
     </div>
 </nav>
