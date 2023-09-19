fa6a5eacc3eb7b3f185b8ca1a27ce28a7d6729b1
Valerio Gallina <valeriudus@hotmail.it>
2023-09-19 17:45:19 +0200

    welcome modificata

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,10 @@
     
     <div class="container mt-3">
         <x-masthead/>
-<<<<<<< HEAD
         Proviamo sto cazzo di push <br>
          Valerio-I lo vedo
-=======
         Proviamo sto cazzo di push
         riesco a pullare e pushare
->>>>>>> 9747a9b19e5e8bff2a3596354943067220f633b5
         <x-search />
         <div class="row align-items-center">
             
