1a36b31bfc9f7f6b2014c380929e1ed9c7e248d0
Davide Imperato <davideimperato95@gmail.com>
2023-09-19 12:45:04 +0200

    menu lingue

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -98,5 +98,13 @@
                 </li>
             @endguest
         </div>
+        <div class="boxLeng">
+            <button id="selectLeng" class="select-leng"><img src="/media/leng-3.png" class="imgSelectLeng" alt=""></button>
+            <div id="lengIT" class="lengIT d-none"><img src="/media/IT.jpeg" class="imgLeng" height="10px" alt=""></div>
+            <div id="lengEN" class="lengEN d-none"><img src="/media/EN.jpeg" class="imgLeng" height="10px" alt=""></div>
+            <div id="lengFR" class="lengFR d-none"><img src="/media/FR.png" class="imgLeng" height="10px" alt=""></div>    
+        </div>
+        
+
     </div>
 </nav>
