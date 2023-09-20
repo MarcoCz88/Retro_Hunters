6b8875f373318ff8bcaf7008c85499b4a1ac87c2
MarcoCz88 <138657270+MarcoCz88@users.noreply.github.com>
2023-09-20 10:15:02 +0200

    Modificato style pagina login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
         <div class="row justify-content-center">
             <div class="col-12 col-md-4 boxLog mx-2 mx-md-0 p-3">
                 <h2 class="text-center">Login</h2>
-                <form action="{{route('login')}}" method="POST">
+                <form action="{{route('login')}}" method="POST" class="d-flex flex-column align-items-center">
                     @csrf
                       <div class="form-group">
                          <label class="form_label">Email</label>
@@ -16,7 +16,7 @@
                          <label class="form_label">Password</label>
                          <input type="password" class="form-control" name="password">
                       </div>
-                      <button type="submit" class="btn btn-primary my-3">{{ __('ui.login')}}</button>
+                      <button type="submit" class="col-5 button-89 my-3 text-dark fs-6" role="button">{{ __('ui.login')}}</button>
                    </form>
             </div>
             <p class="text-center mt-2">{{ __('ui.noAccount')}} <a class="linkLog" href="{{route('register')}}"> {{ __('ui.registered')}}</a></p>
