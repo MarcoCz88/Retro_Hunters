a48785e5ec300abcaeb409d74ec7767df00fd716
MarcoCz88 <138657270+MarcoCz88@users.noreply.github.com>
2023-09-25 16:03:50 +0200

    modifiche style

--- resources/views/revisor/request.blade.php
+++ resources/views/revisor/request.blade.php
@@ -1,20 +1,20 @@
 <x-layout>
     <div class="container">
         <div class="row justify-content-center">
-            <h2 class="col-6 text-center text-primary fs-3 mt-5">Entra nel Team</h2>
+            <h2 class="col-6 text-center text-primary mt-5">Entra nel Team</h2>
         </div>
     </div>
     <form action="{{route('become.revisor', Auth::user())}}" method="get" class="contact-form row justify-content-center align-items-center">
       @csrf
-      <div class="form-field col-lg-6">
-         <label class="label">Inserisci le motivazioni per la tua candidatura: </label>
-         <textarea id="message" name= "presentation_message" class="input-text form-control" type="text" required></textarea>
+      <div class="col-lg-6">
+         <label class="text-primary mt-3 mb-2">Inserisci le motivazioni per la tua candidatura: </label>
+         <textarea id="message" name= "presentation_message" class="input-text" type="text" required></textarea>
         @error('presentation_message')
          {{ $message }}
         @enderror
       </div>
       <div class="row align-items-center justify-content-center">
-         <button class=" col-3 button-892 text-dark" role="button">{{ __('ui.summit') }}</button>
+         <button class=" col-4 col-md-1 button-892 text-dark" role="button">{{ __('ui.summit') }}</button>
       </div>
     </form>
 </x-layout>
