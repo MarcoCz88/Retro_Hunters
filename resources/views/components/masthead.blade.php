09d73ac25e4c2d11d92d04a0c9d3f08aa9dfe5c1
Davide Imperato <davideimperato95@gmail.com>
2023-09-25 09:52:28 +0200

    aggiornamento

--- resources/views/components/masthead.blade.php
+++ resources/views/components/masthead.blade.php
@@ -2,6 +2,7 @@
 <div class="container-fluid">
     <div class="row align-items-top justify-content-center masthead">
         <div class="col-12">
+            <h1>RETRO HUNTERS</h1>
             <x-search />
             
             {{-- <div id="" class="supermario">
--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,6 +1,6 @@
 <div class="container mt-5 pt-5  mb-3">
     <div class="row justify-content-center">
-        <div class="col-10 col-md-6 mt-5 ">
+        <div class="col-10 col-md-6  ">
             <form class="search" action="{{route('announcements.search')}}" method="GET">
                 <input type="search" name="searched" class="searchTerm" placeholder="{{ __('ui.search')}}">
                 <button type="submit" class="searchButton">
@@ -9,7 +9,7 @@
             </form>
 
             <div id="fungo" class="fungo">
-                <img src="/media/fungo.png" height="40px" alt="">
+                {{-- <img src="/media/fungo.png" height="40px" alt=""> --}}
             </div>
         </div>
     </div>
