20a9bbae3738e49063ca0a2d090999c65ac49436
Davide Imperato <davideimperato95@gmail.com>
2023-09-25 14:42:47 +0200

    correzione errori

--- resources/views/livewire/create-announcement.blade.php
+++ resources/views/livewire/create-announcement.blade.php
@@ -1,7 +1,7 @@
 <div id="containerForm" class="container-fluid">
     <div class="row justify-content-center">
         <div id="boxform" class="col-12 boxform col-xl-6 ">
-            <form wire:submit.prevent="store">
+            <form id="formCreate" wire:submit.prevent="store">
                 <div class="col-12">
                     <div class=" mt-3">
                         <label class="form-label text-primary">{{ __('ui.title') }} :</label>
@@ -101,43 +101,43 @@
                     </div>
                 </div>
                 
-                
+                <div class="col-12">
+                    @if (!empty($images))
+                        <div class="row">
+                            <div class="col-12">
+                                <p>{{ __('ui.prevImg') }}: </p>
+                            </div>
+                        </div>
+                        <div class="row justify-content-around ">
+                            @foreach ($images as $key => $image)
+                                <div class="col-12 col-md-3 position-relative boxImg border"
+                                    style="background-image: url('{{ $image->temporaryUrl() }}')">
+                                    {{-- <img class=" img-preview shadow rounded " src="{{ $image->temporaryUrl() }}"> --}}
+                                    {{-- <div  style="backgroud-image:url({{$image->temporaryUrl()}});"></div> --}}
+                                    <button type="button" class=" btnImg" wire:click="removeImage({{ $key }})">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28"
+                                            fill="currentColor" style="color: red" class="bi bi-trash3-fill"
+                                            viewBox="0 0 16 16">
+                                            <path
+                                                d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            @endforeach
+                            @endif
+                        </div>
+                        <div class="col-12 mt-4 text-center">
+                            <button class="button-892 text-dark" role="button">{{ __('ui.summit') }}</button>
+    
+                        </div>
+                </div>
                 
                 
                 
                 
                 
             </form>
-            <div class="col-12">
-                @if (!empty($images))
-                    <div class="row">
-                        <div class="col-12">
-                            <p>{{ __('ui.prevImg') }}: </p>
-                        </div>
-                    </div>
-                    <div class="row justify-content-around ">
-                        @foreach ($images as $key => $image)
-                            <div class="col-12 col-md-3 position-relative boxImg border"
-                                style="background-image: url('{{ $image->temporaryUrl() }}')">
-                                {{-- <img class=" img-preview shadow rounded " src="{{ $image->temporaryUrl() }}"> --}}
-                                {{-- <div  style="backgroud-image:url({{$image->temporaryUrl()}});"></div> --}}
-                                <button type="button" class=" btnImg" wire:click="removeImage({{ $key }})">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28"
-                                        fill="currentColor" style="color: red" class="bi bi-trash3-fill"
-                                        viewBox="0 0 16 16">
-                                        <path
-                                            d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
-                                    </svg>
-                                </button>
-                            </div>
-                        @endforeach
-                        @endif
-                    </div>
-                    <div class="col-12 mt-4 text-center">
-                        <button class="button-892 text-dark" role="button">{{ __('ui.summit') }}</button>
-
-                    </div>
-            </div>
+            
             
         </div>
         
