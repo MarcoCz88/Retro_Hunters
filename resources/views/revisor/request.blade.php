bd6e58fe7ad0a9b0dfefb9c121627192ce381dd5
Piertobia Nigro <ngrptb@gmail.com>
2023-09-25 19:32:22 +0200

    estetica form candidatura

--- resources/views/revisor/request.blade.php
+++ resources/views/revisor/request.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <div class="container">
+    {{-- <div id="containerForm" class="container-fluid">
         <div class="row justify-content-center">
             <h2 class="col-6 text-center text-primary mt-5">{{__('ui.join')}}</h2>
         </div>
@@ -16,5 +16,25 @@
       <div class="row align-items-center justify-content-center">
          <button class=" col-4 col-md-1 button-892 text-dark" role="button">{{ __('ui.summit') }}</button>
       </div>
-    </form>
+    </form> --}}
+
+    <div id="containerForm" class="container-fluid mt-5">
+      <div class="row justify-content-center">
+          <div id="boxform" class="col-12 boxform col-xl-6 ">
+            <form action="{{route('become.revisor', Auth::user())}}" method="get" class="contact-form row justify-content-center align-items-center">
+              @csrf
+              <div class="col-12">
+                 <label class="form-label text-primary">{{__('ui.sendCurriculum')}} </label>
+                 <textarea id="message" name= "presentation_message" class="form-control border border-warning w-100" type="text" required></textarea>
+                @error('presentation_message')
+                 {{ $message }}
+                @enderror
+              </div>
+              <div class="row align-items-center justify-content-center">
+                 <button class="button-892 text-dark w-25" role="button">{{ __('ui.summit') }}</button>
+              </div>
+            </form>
+          </div>
+      </div>
+    </div>
 </x-layout>
